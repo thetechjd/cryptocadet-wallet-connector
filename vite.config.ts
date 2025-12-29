7cd96c5f8ab1fb939e9d5e4955c5a4eb439bdbd7
rakesh9494 <ummadirakesh9494@gmail.com>
2025-12-29 16:53:23 +0530

    removed react import in config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
-    react(),
     dts({
       insertTypesEntry: true,
     }),
